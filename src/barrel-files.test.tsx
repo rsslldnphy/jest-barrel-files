674e74536c4b12f9830a0dccfbb371b9b9b244f7
Russell Dunphy <russell@russelldunphy.com>
2021-11-06 16:52:10 +0000

    Test to demonstrate barrel file issue

---
+++ src/barrel-files.test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import * as Icons from "@mui/icons-material";
+
+test("renders learn react link", () => {
+  render(<Icons.DirectionsRun />);
+});
---
+++ src/no-barrel-files.test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import DirectionsRun from "@mui/icons-material/DirectionsRun";
+
+test("renders learn react link", () => {
+  render(<DirectionsRun />);
+});
